ae885e9f4ff488d7750dc0d16de5e6e4b32b4b8f
Algoberry <72118211+algoberry@users.noreply.github.com>
2022-10-23 15:03:21 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include_once("highlighter.class.php");
 $colorObj = new highlighter();
 $fileContent = $colorObj->applycolor("highlighter.class.php");
 echo $fileContent;
-echo "Wikis are enabled by wiki software, otherwise known as wiki engines. A wiki engine, being a form of a content management system, differs from other web-based systems such as blog software";
 $colorObj->showfilename(false);
 $fileContent = $colorObj->applycolor("index.php");
 echo $fileContent;
